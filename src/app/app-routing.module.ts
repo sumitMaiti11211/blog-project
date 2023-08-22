9dac3096df605818888603f2d9d3909bedfb42a7
Akchat Gupta <akchatgupta0@gmail.com>
2023-08-23 00:58:14 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { BlogComponent } from './blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'blog', pathMatch: 'full' },
+  { path: 'blog', component: BlogComponent },
+  { path: 'portfolio', component: PortfolioComponent }, // Add your portfolio route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
